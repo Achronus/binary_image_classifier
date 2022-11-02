ab4aa54fa13ea2eb24763fcb30e729f127febeee
Ryan Partridge <rpartridge101@yahoo.co.uk>
2022-11-02 21:39:27 +0000

    Modified functionality
    - Updated LabelCleaner class
    - Added FilepathHandler class
    - Removed SingleDataCleaner class
    - Added respective parameters to parameters.toml

--- app/cleaning.py
+++ app/cleaning.py
@@ -1,115 +1,161 @@
+from pathlib import Path
+
 import pandas as pd
+
+
+def clean_data() -> pd.DataFrame:
+    """Cleans label (if applicable) and image data. Removing duplicate and invalid data, and updates image names
+    and moves them to their respective label folder."""
+    # Check for labels and data directories
+    pass
+
+
+class FilepathHandler:
+    """Handles image filepaths related to the data folder."""
+    def __init__(self) -> None:
+        self.data_dir = Path(f'{Path.cwd()}/data')
+        self.col_filepath = 'img_filepaths'
+
+    def get_filepaths(self, img_extension: str) -> pd.DataFrame:
+        """
+        Creates a pandas.DataFrame containing the data directory image filepaths based on the given img extension.
+
+        :param img_extension: (string) the file extension for the images in the data folder
+        :return: a pandas.DataFrame containing the data directory image filepaths
+        """
+        data_dir_filepaths = [str(item) for item in list(self.data_dir.glob(f'*.{img_extension}'))]
+        return pd.DataFrame({self.col_filepath: data_dir_filepaths})
+
+    def create_filepaths(self, img_filenames: pd.Series) -> pd.Series:
+        """
+        Combines a pandas.Series of data with the data directory filepath to create new filepaths.
+
+        :param img_filenames: (pandas.Series) a column of data containing image filenames
+        :return: a pandas.Series containing new image filepaths, merging the data directory filepath and image filenames
+        """
+        return str(self.data_dir) + '\\' + img_filenames
 
 
 class LabelCleaner:
     """
-    A class dedicated to cleaning training and testing labels Pandas DataFrames.
+    Cleans existing label data files using Pandas DataFrames.
 
-    :param train_labels: (pd.DataFrame) a pandas DataFrame containing the training label data
-    :param test_labels: (pd.DataFrame) a pandas DataFrame containing the test label data
-    :param labels: (list[str]) a list of unique class names. E.g., ['civilian', 'military']
+    :param dataset_filenames: (dict[string, string]) a dictionary containing the train and test label data filenames.
+                              These files must be 'csv' files and stored in a 'labels' folder in the root directory.
+                              Valid keys - ['train_labels', 'test_labels']
+    :param separator: (string, optional) delimiter to use for filenames. Refer to 'pandas.read_csv' 'sep' parameter for
+                      accepted separators. Default is ','
     :param filename_column: (string, optional) name of the filename column. Default is 'filename'
     :param label_column: (string, optional) the column name to look for the label. Default is 'class'
+    :param custom_columns: (list[string], optional) a list of column names for the cleaned data.
+                           Default is ['filename', 'class', 'type']
     """
-
-    def __init__(self, train_labels: pd.DataFrame, test_labels: pd.DataFrame, labels: list[str],
+    def __init__(self, dataset_filenames: dict[str, str], custom_columns: list[str] = None, separator: str = ',',
                  filename_column: str = 'filename', label_column: str = 'class') -> None:
-        self.labels = list(set(labels))
-        self.label_column = label_column
+        self.labels_train = pd.read_csv(f'labels/{dataset_filenames["train_labels"]}', sep=separator)
+        self.labels_test = pd.read_csv(f'labels/{dataset_filenames["test_labels"]}', sep=separator)
+        self.cleaned_data = None  # Updated after calling '.update()'
 
-        self.single_data_cleaner = SingleDataCleaner(
-            train_labels,
-            test_labels,
-            labels=labels,
-            filename_column=filename_column,
-            label_column=label_column
-        )
+        self.col_filename = filename_column
+        self.col_label = label_column
+        self.col_custom = ['filename', 'class', 'type'] if custom_columns is None else custom_columns
 
-    def clean_data(self) -> pd.DataFrame:
+    def merge_data(self) -> pd.DataFrame:
+        """Combines the train and test label data into a single Pandas DataFrame, adding a column for its type
+        (train or test).
+        :return: a reduced DataFrame with the train and test label data containing the filename, label, and type columns
         """
-        Preprocesses the class data using various helper functions.
+        self.labels_train['type'] = 'train'
+        self.labels_test['type'] = 'test'
+        return pd.concat([self.labels_train, self.labels_test])[self.col_custom].reset_index(drop=True)
 
-        :return: a pd.DataFrame containing the updated training and test label data
+    def update(self, labels: list[str], img_extension: str = 'jpg') -> pd.DataFrame:
         """
-        self.single_data_cleaner.remove_individual_duplicates()
-        data_labels = self.single_data_cleaner.merge_labels().reset_index(drop=True)
+        Preprocesses the label data, concatenating it together, updating the class names, removing multi-labelled
+        instances and missing labelled values.
 
-        for label in self.labels:
-            data_labels = self.__update_class_name(data_labels, label)
+        :param labels: (list[string]) a list of unique class labels
+        :param img_extension: (string) the file extension for the images in the data folder. Default is 'jpg'
+        :return: a pandas.DataFrame containing the updated training and test label data
+        """
+        labels = list(set(labels))
+        all_labels = self.merge_data()
+        print(f'Total records: {len(all_labels)}')
 
-        return data_labels
+        # Update class label names
+        for label in labels:
+            all_labels = self.update_class_name(all_labels, label)
 
-    def __update_class_name(self, data: pd.DataFrame, word: str) -> pd.DataFrame:
+        # Remove invalid and duplicates
+        all_labels = self.remove_multi_label_data(all_labels)
+        all_labels = self.remove_duplicates(all_labels)
+        all_labels = self.remove_data_without_labels(all_labels, img_extension)
+
+        self.cleaned_data = all_labels
+        print(f'Total records after cleaning: {len(all_labels)}')
+        return all_labels
+
+    def update_class_name(self, data: pd.DataFrame, word: str) -> pd.DataFrame:
         """
         Updates class names in 'data' by removing text before and after the given word.
 
+        :param data: (pandas.DataFrame) a DataFrame containing the training and testing label data
         :param word: (string) the new class label name
         :return: an updated pd.DataFrame
         """
-        data[self.label_column] = data[self.label_column].replace({
-            f'.*(?={word})|(?<={word}).*$': ''
-        }, regex=True)
+        data[self.col_label] = data[self.col_label].replace({f'.*(?={word})|(?<={word}).*$': ''}, regex=True)
         return data
 
-
-class SingleDataCleaner:
-    """A helper class for performing initial cleaning of the training and test labels separately.
-
-    :param train_labels: (pd.DataFrame) a pandas DataFrame containing the training label data
-    :param test_labels: (pd.DataFrame) a pandas DataFrame containing the test label data
-    :param labels: (list[str]) a list of unique class names. E.g., ['civilian', 'military']
-    :param filename_column: (string) name of the filename column.
-    :param label_column: (string) the column name to look for the label.
-    """
-    def __init__(self, train_labels: pd.DataFrame, test_labels: pd.DataFrame, labels: list[str],
-                 filename_column: str, label_column: str) -> None:
-        self.train_labels = train_labels
-        self.test_labels = test_labels
-
-        self.labels = list(set(labels))
-        self.filename_column = filename_column
-        self.label_column = label_column
-
-    def get_label_data(self, data: pd.DataFrame, label: str) -> pd.DataFrame:
+    def remove_multi_label_data(self, data: pd.DataFrame) -> pd.DataFrame:
         """
-        Retrieves data from the class dataframe based on the given label and column name.
+        Removes multi-labelled data instances by grouping by the filename and counting the class instances. Filenames
+        with more than one label are then removed from the input data.
 
-        :param data: (pd.DataFrame) the training or testing labels DataFrame
-        :param label: (string) a single class label found in the data
-        :return: a new pd.DataFrame containing all rows and columns for the desired class label
+        :param data: (pandas.DataFrame) a DataFrame containing the training and testing label data
+        :return: an updated pandas.DataFrame without multi-labelled data instances
         """
-        return data[data[self.label_column].str.contains(label)]
+        data = data.reset_index(drop=True)  # Reset indices first
+        label_counts = data.groupby(self.col_filename).apply(lambda x: len(x[self.col_label].unique())).reset_index()
+        multi_labelled = label_counts[label_counts[0] > 1]
+        print(f'Multi-labelled items removed: {len(multi_labelled)}')
+        return data.drop(multi_labelled.index)
 
-    def merge_labels(self) -> pd.DataFrame:
+    def remove_duplicates(self, data: pd.DataFrame) -> pd.DataFrame:
         """
-        Merges the train and test label datasets into a single DataFrame.
+        Removes duplicate data from a given DataFrame of label data.
 
-        :returns: a pd.DataFrame containing a combination of the train and test labels
+        :param data: (pandas.DataFrame) a DataFrame containing the training and testing label data
+        :return: an updated pandas.DataFrame without duplicate filename instances
         """
-        return pd.concat([self.train_labels, self.test_labels]).drop_duplicates(self.filename_column, keep='last')
+        sorted_data = data.sort_values('type', ascending=False)  # train on top, test on bottom
+        no_dupes = sorted_data.drop_duplicates(self.col_filename, keep='last').reset_index(drop=True)  # Keep test data
+        print(f'Duplicates removed: {len(sorted_data) - len(no_dupes)}')
+        return no_dupes
 
-    def remove_individual_duplicates(self) -> None:
+    def remove_data_without_labels(self, data: pd.DataFrame, img_extension: str) -> pd.DataFrame:
         """
-        Removes overlapping class names (images with both classes) and duplicate filenames from each dataset,
-        respectively. Also, adds a 'type' column, specifying whether the data is for training or testing.
-        Updates the class attributes - self.train_labels, self.test_labels.
+        Compares filenames in the given DataFrame with filenames in the data directory, removing records from the
+        DataFrame that are not present in both.
+
+        :param data: (pandas.DataFrame) a DataFrame containing the training and testing label data
+        :param img_extension: (string) the file extension for the images in the data folder
+        :return: an updated pandas.DataFrame without missing label data
         """
-        for key, dataset in {'train_labels': self.train_labels, 'test_labels': self.test_labels}.items():
-            filenames_label_a = self.get_label_data(dataset, self.labels[0])
-            filenames_label_b = self.get_label_data(dataset, self.labels[1])
+        # Store image filepaths from the data folder in a DataFrame
+        fp_handler = FilepathHandler()
+        col_filepath = fp_handler.col_filepath
+        data_dir_filepaths = fp_handler.get_filepaths(img_extension)
 
-            # Clean data (overlapping class names and duplicate filename)
-            overlap_duplicates = filenames_label_b[
-                filenames_label_b[self.filename_column].isin(filenames_label_a[self.filename_column])
-            ].drop_duplicates(self.filename_column)
-            filenames_unique = dataset.drop_duplicates(self.filename_column)
-            filenames_clean = filenames_unique[
-                ~filenames_unique[self.filename_column].isin(overlap_duplicates[self.filename_column])
-            ].reset_index(drop=True)
+        # Create img filepaths in data DataFrame
+        updated_data = data.copy()
+        updated_data[col_filepath] = fp_handler.create_filepaths(data[self.col_filename])
 
-            # Add data type column
-            temp = filenames_clean.copy()
-            temp['type'] = key.split('_')[0]
+        # Remove invalid records from DataFrame
+        updated_data = data_dir_filepaths.merge(updated_data, how='left', on=[col_filepath]).dropna().reset_index(drop=True)
+        print(f'Data without labels removed: {len(data) - len(updated_data)}')
+        return updated_data
 
-            print(f'{key} overlap duplicates removed: {len(overlap_duplicates)}')
-            setattr(self, key, temp)
+
+class ImageCleaner:
+    """A class dedicated to modifying image data inside the data folder. Complements the LabelCleaner."""
+    pass
